87e7400bfcb16acfa9f7d09759312a63814b354a
Kasra Naderi <kasra@yuser.co>
2022-06-15 13:27:09 -0400

    initialize tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,11 @@ const Home: NextPage = () => {
         <meta name="description" content="A web3 utility tool!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <button
+        className="px-7 py-4 text-xl rounded-xl bg-yellow-300 animate-pulse"
+      >
+        Login using Metamask
+      </button>
     </div>
   )
 }
