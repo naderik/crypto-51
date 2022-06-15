a9c5cdbb4471dd13b361ba6f528565044ea6bf4e
Kasra Naderi <kasra@yuser.co>
2022-06-15 13:19:18 -0400

    add moralis provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (
+    <MoralisProvider
+      appId={process.env.NEXT_PUBLIC_APP_ID ?? "appId undefined"}
+      serverUrl={process.env.NEXT_PUBLIC_SERVER_URL ?? "serverUrl undefined"}
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
