411f4ad20712e1b6fca5736736d2abf164d8e0a4
Kasra Naderi <kasra@yuser.co>
2022-06-15 13:55:23 -0400

    change to styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className="content-center" >
+    <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12" >
       <Head>
         <title>Crypto-51</title>
         <meta name="description" content="A web3 utility tool!" />
