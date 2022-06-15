1c311585e1568e16cab241e33fc904935c7a9d6d
Kasra Naderi <kasra@yuser.co>
2022-06-15 14:05:33 -0400

    add pulse animation

--- components/MetaMaskLogin.tsx
+++ components/MetaMaskLogin.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 
 function MetaMaskLogin() {
     return (
-        <button type="button" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2">
+        <button type="button" className="animate-pulse text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2">
             <Image className="w-6 h-5 mr-2 -ml-1" src='/assets/MetaMask.svg' width='50' height='50' alt='metamask fox' />
             Connect with MetaMask
         </button>
