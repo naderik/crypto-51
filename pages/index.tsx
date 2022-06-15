ab638c0050ddc9f15335c95626ca589022b335fb
Kasra Naderi <kasra@yuser.co>
2022-06-15 13:44:54 -0400

    connect with metamask button looks better

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,11 +11,11 @@ const Home: NextPage = () => {
         <meta name="description" content="A web3 utility tool!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <button
-        className="px-7 py-4 text-xl rounded-xl bg-yellow-300 animate-pulse"
-      >
-        Login using Metamask
+      <button type="button" className="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-600 dark:bg-gray-800 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 mr-2 mb-2">
+        <Image className="w-6 h-5 mr-2 -ml-1" src='/assets/MetaMask.svg' width='50' height='50' alt='metamask fox' />
+        Connect with MetaMask
       </button>
+
     </div>
   )
 }
