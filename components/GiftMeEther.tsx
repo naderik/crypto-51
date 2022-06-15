6628d5affaebe536add16b5f27be2e100d76a537
Kasra Naderi <kasra@yuser.co>
2022-06-15 18:48:28 -0400

    add eth transfer function

--- components/GiftMeEther.tsx
+++ components/GiftMeEther.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { useWeb3Transfer } from "react-moralis";
+import Moralis from "moralis";
 
 function GiftMeEther() {
+
+    const { fetch, error, isFetching } = useWeb3Transfer({
+        type: "native",
+        amount: Moralis.Units.ETH(0.5),
+        receiver: "0xc4c2e4B9B212be4f41382389a6dB2db2C8427f04",
+    });
     return (
         <div className="h-screen w-screen flex flex-col items-center justify-center">
             <button
+                onClick={() => fetch()} disabled={isFetching}
                 className="px-7 py-4 mb-5 text-xl rounded-xl text-gray-900 bg-white hover:bg-gray-100"
             >
                 Send ETH!
--- components/MetaMaskLogin.tsx
+++ components/MetaMaskLogin.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image'
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import Moralis from "moralis";
 import { useMoralis } from "react-moralis";
 
 
