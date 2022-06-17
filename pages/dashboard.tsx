46f55d6676b9cf935076a426fde319dc3a7b458e
Kasra Naderi <kasra@yuser.co>
2022-06-16 21:15:41 -0400

    move wallet to separate component

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -3,33 +3,21 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import Moralis from "moralis";
 import { useMoralis } from "react-moralis";
-import GiftMeEther from '../components/GiftMeEther';
+import Wallet from '../components/Wallet';
 
-/**
- * 
- * Dashboard page.
- * 
- */
+
 const Dashboard: NextPage = () => {
-
-
-
-    const { isAuthenticated, logout } = useMoralis();
+    const { isAuthenticated } = useMoralis();
     const router = useRouter();
 
     useEffect(() => {
         if (!isAuthenticated) router.replace("/");
     }, [isAuthenticated, router]);
 
+
     return (
         <div className="h-screen w-screen flex flex-col items-center justify-center">
-            <GiftMeEther />
-            <button
-                onClick={logout}
-                className="px-7 py-4 text-xl rounded-xl text-gray-900 bg-white hover:bg-gray-100"
-            >
-                Logout
-            </button>
+            <Wallet />
         </div>
     );
 }
