c816237a8b2eee70763fcd76030eb0aeb8739461
Kasra Naderi <kasra@yuser.co>
2022-06-23 14:07:13 -0400

    change fonts

--- components/Wallet.tsx
+++ components/Wallet.tsx
@@ -68,7 +68,7 @@ const Wallet: NextComponentType<Props> = () => {
                 </div>
             ) : null}
             <div className="px-2 py-2 flex-auto">
-                <span className="px-4 py-4 font-bold text-2xl flex flex-row text-gray-900 rounded-xl bg-white border-double border-2 border-gray-500">
+                <span className="px-4 py-4 font-mono font-bold text-2xl flex flex-row text-gray-900 rounded-xl bg-white border-double border-2 border-gray-500">
                     <FaWallet className="mx-4" size='4rem' />
                     Welcome To Your Ethereum Wallet !
                 </span>
@@ -77,19 +77,19 @@ const Wallet: NextComponentType<Props> = () => {
                 <button className="px-4 py-4 my-2 text-gray-900 rounded-xl bg-white hover:bg-gray-100 border-solid border-2 border-gray-500 "
                     onClick={() => handleCopyAddress()}
                 >
-                    <span className="font-bold text-2xl flex flex-row">
+                    <span className="font-bold font-mono text-2xl flex flex-row">
                         Wallet Address: {addressToShow}
                         <VscCopy className="mx-4" size='2rem' />
                     </span>
                 </button>
             </div>
             <div className="px-2 py-2 flex-auto">
-                <span className="px-4 py-4 my-2  text-2xl font-bold text-gray-900 rounded-xl bg-white hover:bg-gray-100 border-solid border-2 border-gray-500  ">
+                <span className="px-4 py-4 my-2  text-2xl font-mono font-bold text-gray-900 rounded-xl bg-white hover:bg-gray-100 border-solid border-2 border-gray-500  ">
                     Wallet Balance: {balance} ETH
                 </span>
             </div>
             <div className="px-2 py-2 flex-auto">
-                <button className="px-4 py-4 my-2 font-bold text-2xl rounded-xl text-gray-900 bg-white hover:bg-gray-100 border-solid border-2 border-gray-500 "
+                <button className="px-4 py-4 my-2 font-mono font-bold text-2xl rounded-xl text-gray-900 bg-white hover:bg-gray-100 border-solid border-2 border-gray-500 "
                     onClick={logout}
                 >
                     Logout
