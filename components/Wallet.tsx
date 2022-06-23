af140a05bbbebcb5a65c243b35d4ff01eca6aea6
Kasra Naderi <kasra@yuser.co>
2022-06-23 13:40:16 -0400

    clean up styling

--- components/Wallet.tsx
+++ components/Wallet.tsx
@@ -67,27 +67,28 @@ const Wallet: NextComponentType<Props> = () => {
                 </div>
             ) : null}
 
-            <div className="first-letter:px-4 py-4 mx-4 my-4 flex-auto">
-                <button className="px-4 py-4 rounded-xl text-gray-900 bg-white hover:bg-gray-100"
+            <div className="px-2 py-2 flex-auto">
+                <button className="px-4 py-4 text-gray-900 rounded-xl bg-white hover:bg-gray-100 "
                     onClick={() => handleCopyAddress()}
                 >
-                    <span className="px-4 py-4 font-bold text-2xl flex flex-row">
+                    <span className="font-bold text-2xl flex flex-row">
                         Wallet Address: {addressToShow}
                         <VscCopy className="mx-4" size='2rem' />
                     </span>
                 </button>
             </div>
-            <div className="px-4 py-4 flex-auto">
-                <span className="text-gray-900 text-base">
+            <div className="px-2 py-2 flex-auto">
+                <span className="px-4 py-4 text-2xl font-bold text-gray-900 rounded-xl bg-white hover:bg-gray-100 ">
                     Wallet Balance: {balance} ETH
                 </span>
             </div>
-            <button
-                onClick={logout}
-                className="px-4 py-4 mx-4 my-4 font-bold text-2xl rounded-xl text-gray-900 bg-white hover:bg-gray-100"
-            >
-                Logout
-            </button>
+            <div className="px-2 py-2 flex-auto">
+                <button className="px-4 py-4 font-bold text-2xl rounded-xl text-gray-900 bg-white hover:bg-gray-100"
+                    onClick={logout}
+                >
+                    Logout
+                </button>
+            </div>
         </div>
     )
 }
